295afe1fb3b70ff36c2940df94403249a1377ab5
mint <mint@mint-vbox>
2018-04-12 14:30:28 +0200

    Versión 1.1 Añadida una línea de separación.

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -8,14 +8,15 @@
  *
  * @author mint
  */
-public class main {
+public class HolaMundo {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola, muindo");
+        System.out.println("Hola, mundo");
+        System.out.println(".............");
     }
     
 }
