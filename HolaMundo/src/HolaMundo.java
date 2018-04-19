9dd056f2336bfbe0f99595db077ece7e14e1fe34
carlos <carlos@UbuntuExt>
2018-04-19 19:44:00 +0200

    Versión 1.3

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -21,6 +21,8 @@ public class HolaMundo {
         /**Cambio provisional*/
         System.out.println("Hola, món");
         System.out.println(".............");
+        /**Cambio por el user2*/
+        System.out.println("Modificado por user2");
     }
     
 }
